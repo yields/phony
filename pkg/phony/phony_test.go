e9014e49924668e7633ea5228d4fed4913913088
Peter Aba <peter.aba@westwing.de>
2016-03-30 10:58:20 +0200

    Expected value should come before the actual value

--- pkg/phony/phony_test.go
+++ pkg/phony/phony_test.go
@@ -12,19 +12,19 @@ import (
 func TestGet(t *testing.T) {
 	a, _ := Get("name")
 	b, _ := Get("name")
-	assert.NotEqual(t, a, "")
-	assert.NotEqual(t, b, "")
+	assert.NotEqual(t, "", a)
+	assert.NotEqual(t, "", b)
 }
 
 func TestGetWithArgsBehavesAsGetByDefault(t *testing.T) {
 	a, _ := GetWithArgs("name", []string{})
-	assert.NotEqual(t, a, "")
+	assert.NotEqual(t, "", a)
 }
 
 func TestSmartdouble(t *testing.T) {
 	a, err := GetWithArgs("smartdouble", []string{"100", "10000", "1000", "1000000"})
 	assert.Nil(t, err)
-	assert.NotEqual(t, a, "")
+	assert.NotEqual(t, "", a)
 
 	num, err := strconv.ParseFloat(a, 64)
 	assert.Nil(t, err)
@@ -82,12 +82,12 @@ func TestSmartdate(t *testing.T) {
 
 func TestEmpty(t *testing.T) {
 	a, _ := Get("foo")
-	assert.Equal(t, a, "")
+	assert.Equal(t, "", a)
 }
 
 func TestAll(t *testing.T) {
 	for _, p := range List() {
 		a, _ := Get(p)
-		assert.NotEqual(t, a, "")
+		assert.NotEqual(t, "", a)
 	}
 }
